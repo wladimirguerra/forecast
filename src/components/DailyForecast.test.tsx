229ff75ad6f5b6688f4d7e41a0aaf3b0e82d3ce8
WAGuerra <wladimir.guerra@gmail.com>
2022-07-14 09:58:40 -0300

    Fix DailyForecast.test.tsx bug

--- src/components/DailyForecast.test.tsx
+++ src/components/DailyForecast.test.tsx
@@ -3,7 +3,7 @@ import DailyForecastPanel from "./DailyForecastPanel";
 import { DailyForecast } from "../features/forecast/forecastSlice";
 
 const FORECAST_MOCK: DailyForecast = {
-  day: new Date(),
+  day: new Date(2022, 6, 14),
   humidity: 60,
   mode: 10,
   dayTemperature: 21,
